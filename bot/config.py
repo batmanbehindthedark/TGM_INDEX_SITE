6e83a9d25fea7fdd57242e2983a40909d11c52c6
ACHYUTH KUMAR V <129989819+batmanbehindthedark@users.noreply.github.com>
2024-05-19 23:05:48 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -10,7 +10,7 @@ class Telegram:
     BOT_TOKEN = getenv("BOT_TOKEN", "")
     PORT = int(getenv("PORT", 8080))
     SESSION_STRING = getenv("SESSION_STRING", "")
-    BASE_URL = getenv("BASE_URL").rstrip('/')
+    BASE_URL = getenv("BASE_URL"), "")
     DATABASE_URL = getenv("DATABASE_URL")
     AUTH_CHANNEL = [channel.strip() for channel in getenv("AUTH_CHANNEL").split(",")]
     THEME = getenv("THEME", "quartz").lower()
